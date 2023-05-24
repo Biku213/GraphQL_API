c0007ac4e8ea3c0730061af93186ffdc0e45516e
Biku213 <kunwarbindu220@gmail.com>
2023-05-24 16:40:39 +0545

    added

---
+++ datasource/ethDatasource.js
@@ -0,0 +1,30 @@
+const { RESTDataSource } = require("apollo-datasource-rest");
+
+// Vitalik's Ethereum Address
+const eth_address = "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045";
+
+// Etherscan Data Source Class
+class EtherDataSource extends RESTDataSource {
+  constructor() {
+    super();
+    this.baseURL = "https://api.etherscan.io/api";
+  }
+
+  async etherBalanceByAddress() {
+    return this.get(`?module=account&action=balance&address=${eth_address}&tag=latest&apikey=UNQ91MP57AVJ85N2UYEGD1HE85DDHYFA87`, {}, {
+      headers: {
+        accept: "application/json",
+      },
+    });
+  }
+
+  async totalSupplyOfEther() {
+    return this.get(`?module=stats&action=ethsupply&apikey=UNQ91MP57AVJ85N2UYEGD1HE85DDHYFA87`, {}, {
+      headers: {
+        accept: "application/json", 
+      },
+    });
+  }
+}
+
+module.exports = EtherDataSource;
